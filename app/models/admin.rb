3e61c372f0894f6744831d205d532db7d75b4ceb
Breon Knight <Breonknight7@gmail.com>
2016-02-29 20:13:23 -0800

    added paperclip and model associations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,12 @@
 class Admin < ActiveRecord::Base
+
+  #bcrypt secure password
+  has_secure_password
+
+  #for paperclip usage
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  #admin has many blogs
+  has_many :blogs
 end
---
+++ db/migrate/20160301040916_add_avatar_columns_to_blogs.rb
@@ -0,0 +1,9 @@
+class AddAvatarColumnsToBlogs < ActiveRecord::Migration
+  def up
+    add_attachment :blogs, :avatar
+  end
+
+  def down
+    remove_attachment :blogs, :avatar
+  end
+end
