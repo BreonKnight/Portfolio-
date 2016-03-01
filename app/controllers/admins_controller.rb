026b74bbc2c3abe488e82fbb294efbd546b27e4b
Breon Knight <Breonknight7@gmail.com>
2016-02-29 20:59:05 -0800

    added sessions controller and a mockup signup page, there will be no sign up page once finished just testing

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,4 +3,15 @@ class AdminsController < ApplicationController
     @admins = Admin.all
     render :index
   end
+
+  def show
+    @admin = Admin.find_by_id(params[:id])
+  end
+
+  def create
+    admin_params = params.require(:admin).permit(:first_name, :last_name, :email, :github, :twitter, :city, :state, :password, :avatar)
+    
+    @admin = Admin.create(admin_params)
+    redirect_to root_path
+  end
 end
