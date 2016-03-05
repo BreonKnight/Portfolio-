ccde676f4dc0c01c2d2b615aa127540e947dca66
Breon Knight <Breonknight7@gmail.com>
2016-03-05 14:03:13 -0800

    added controller methods

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,44 @@
 class BlogsController < ApplicationController
+
+  def index
+    @blogs = Blog.all
+    render :index
+  end
+
+  def new
+    @blog = Blog.new
+    render :new
+  end
+
+  def show
+    blog_id = params[:id]
+    @blog = Blog.find_by_id(blog_id)
+    render :show
+  end
+
+  def create
+    blog_params = params.require(:blog).permit(:title, :body, :avatar).merge(admin_id: current_admin.id)
+    @blog = Blog.create(blog_params)
+    redirect_to root_path
+  end
+
+  def edit
+    blog_id = params[:id]
+    @blog = Blog.find_by_id(blog_id)
+    render :show
+  end
+
+  def update
+    blog_id = params[:id]
+    blog = Blog.find_by_id(blog_id)
+    blog_params = params.require(:blog).permit(:title, :body, :avatar)
+    blog.update_attributes(blog_params)
+    redirect_to root_path
+    render :update
+  end
+
+  def destroy
+
+  end
+  
 end
